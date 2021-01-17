bb68c0203fdb393c415b9e98d884e9dd3c9c1e36
Roberto Rosa <roberto.rosa7@gmail.com>
2021-01-17 10:52:57 -0300

    open notification native

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 ###### 12/01/2021
 
 - [x] Projeto Begins - introdução ao Electron
+
+###### 17/01/2021
+
+- [x] Instalação das dependências
+- [x] Preparação para o build em Linux platform
--- primeiro-app/main.js
+++ primeiro-app/main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Notification } = require("electron");
+const fs = require("fs");
+const root = fs.readdirSync("./");
+const S3 = require("aws-sdk/clients/s3");
+
+// console.log(S3); // show all methods and service from nodes
+// console.log(root); // print all list dir
 
 let win;
 
@@ -7,12 +13,15 @@ function createWindow() {
   // new instance of window
   win = new BrowserWindow({
     width: 1358,
-    height: 760,
+    height: 768,
     backgroundColor: "#FFFFFF",
     icon: `file://${__dirname}/dist/assets/logo-angular.png`,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
 
-  console.log(`file://${__dirname}/dist/assets/favicon.ico`)
+  // console.log(`file://${__dirname}/dist/assets/favicon.ico`)
 
   win.loadURL(`file://${__dirname}/dist/index.html`);
 
@@ -25,20 +34,36 @@ function createWindow() {
   });
 }
 
+// create a new notification
+function createNotification() {
+  const notification = {
+    title: "Simple notification",
+    body: "This is a simple notification from Electron App",
+  };
+  new Notification(notification).show();
+}
+
 // create window on electrion initialization
-app.on("ready", createWindow);
+// app.on("ready", createWindow);
+app.whenReady().then(createWindow).then(createNotification);
 
 // Quit when all windows are closed
-app.on("window-all-closed", function () {
+app.on("window-all-closed", () => {
   // on macOS specific close process
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on("activate", function () {
-  // on macOS specific close process
-  if (win === null) {
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+// app.on("activate", function () {
+//   // on macOS specific close process
+//   if (win === null) {
+//     createWindow();
+//   }
+// });
