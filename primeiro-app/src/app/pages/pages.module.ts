a9760b472908a186936c376c1d660122f4d7c1bc
Roberto Rosa <roberto.rosa7@gmail.com>
2021-01-27 10:10:51 -0300

    component grid

--- primeiro-app/src/app/pages/pages.module.ts
+++ primeiro-app/src/app/pages/pages.module.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from "./home/home.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SettingsComponent } from './settings/settings.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { GridComponent } from '../components/grid/grid.component';
 @NgModule({
   declarations: [
     HomeComponent,
     DashboardComponent,
     SettingsComponent,
+    GridComponent,
 
   ],
   imports: [
