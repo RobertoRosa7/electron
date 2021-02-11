8e22b44f128250b1281658e3f0216ebb39c43f5e
Roberto Rosa <roberto.rosa7@gmail.com>
2021-02-11 14:36:36 -0300

    include component on dashboard

--- primeiro-app/src/app/pages/dashboard/dashboard.component.ts
+++ primeiro-app/src/app/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Store } from '@ngrx/store'
-import * as actionDashboard from '../../actions/dashboard.actions'
 
 @Component({
   selector: 'app-dashboard',
@@ -31,53 +29,16 @@ export class DashboardComponent implements OnInit {
       icon: 'settings'
     },
   ]
-  public cards: any[] = [
-    {
-      title: 'Entrada',
-      icon: 'monetization_on',
-      value: 0,
-      type: 'incoming'
-    },
-    {
-      title: 'Saída',
-      icon: 'money_off',
-      value: 0,
-      type: 'outcoming'
-    },
-    {
-      title: 'Consolidado',
-      icon: 'account_balance',
-      value: 0,
-      type: 'consolidado'
-    }
-  ]
 
   public searchTerms: string | number
 
   constructor(
-    private _store: Store,
     private _router: Router
   ) {
-    this._store.dispatch(actionDashboard.INIT())
   }
 
   public ngOnInit(): void {
-    this._store.select(({ dashboard }: any) => ({ ...dashboard }))
-      .subscribe(dash => {
-        this.cards.forEach(value => {
-          switch (value.type) {
-            case 'incoming':
-              value.value = dash.total_credit
-              break
-            case 'outcoming':
-              value.value = dash.total_debit
-              break
-            case 'consolidado':
-              value.value = dash.total_consolidado
-              break
-          }
-        })
-      })
+    
   }
 
   public onSubmit(): void {
--- primeiro-app/src/app/pages/dashboard/main/main.component.ts
+++ primeiro-app/src/app/pages/dashboard/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actionDashboard from '../../../actions/dashboard.actions'
 
 @Component({
   selector: 'app-main',
@@ -6,10 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  public cards: any[] = [
+    // {
+    //   title: 'Entrada',
+    //   icon: 'monetization_on',
+    //   value: 0,
+    //   type: 'incoming'
+    // },
+    // {
+    //   title: 'Saída',
+    //   icon: 'money_off',
+    //   value: 0,
+    //   type: 'outcoming'
+    // },
+    {
+      title: 'Consolidado',
+      icon: 'account_balance',
+      value: 0,
+      type: 'consolidado'
+    }
+  ]
 
-  constructor() { }
+  constructor(
+    private _store: Store
+  ) {
+    this._store.dispatch(actionDashboard.INIT())
+  }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this._store.select(({ dashboard }: any) => ({ ...dashboard }))
+      .subscribe(dash => {
+        this.cards.forEach(value => {
+          switch (value.type) {
+            case 'incoming':
+              value.value = dash.total_credit
+              break
+            case 'outcoming':
+              value.value = dash.total_debit
+              break
+            case 'consolidado':
+              value.value = dash.total_consolidado
+              break
+          }
+        })
+      })
   }
 
 }
