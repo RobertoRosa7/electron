c71841b14f7c4a92d28279ca9dc5c00e4ea9897c
Roberto Rosa <roberto.rosa7@gmail.com>
2021-02-15 15:50:54 -0300

    integrando constantes para chamada do endpoint

---
+++ primeiro-app/src/app/services/constants.ts
@@ -0,0 +1,25 @@
+import { environment } from '../../environments/environment'
+
+export class Constants {
+  private readonly host: string
+
+  constructor() {
+    if (environment) {
+      this.host = 'http://localhost:5000/'
+    } else {
+      this.host = 'http://localhost:5000/'
+    }
+  }
+
+  public readonly paths: any = {
+    fetch_registers: 'dashboard/fetch_registers',
+    new_register: 'dashboard/new_register'
+  }
+
+  public get(key: string, host?: string): string {
+    host = host ? host : this.host
+    const path = host + this.paths[key]
+    if (path === undefined) throw new Error("Couldnt find " + key + " in paths")
+    return path
+  }
+}
